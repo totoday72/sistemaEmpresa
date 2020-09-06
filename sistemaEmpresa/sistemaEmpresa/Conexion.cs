55b4725b0a0695bf55a5f7a1982af87958772329
anibal <gonibal15@gmail.com>
2020-09-05 19:31:52 -0600

    Insersion de Proveedores, categorias y Empleados

--- sistemaEmpresa/sistemaEmpresa/Conexion.cs
+++ sistemaEmpresa/sistemaEmpresa/Conexion.cs
@@ -5,6 +5,7 @@ using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace sistemaEmpresa
 {
@@ -47,6 +48,66 @@ namespace sistemaEmpresa
             }
 
         }
+
+
+        public void AgregarProveedor(String nombre, String razon_social, String direccion, String telefono, String nit, String correo)
+        {
+            DataTable tabla = new DataTable();
+            try
+            {
+                conexionstring.Open();
+                OleDbDataAdapter adaptador = new OleDbDataAdapter("Insert into PROVEEDORES (nombre, razon_social,direccion, telefono, nit, email) values ('" + nombre + "', '" + razon_social + "', '" + direccion + "', '" + telefono + "', '" + nit + "', '" + correo + "');", conexionstring);
+                adaptador.Fill(tabla);
+                conexionstring.Close();
+                MessageBox.Show("Proveedor Creado con exito");
+            }
+            catch (System.Exception error)
+            {
+                MessageBox.Show("Ha habido un error en su conexión, intentelo más tarde");
+            }
+
+        }
+
+
+        public void AgregarCategoria(String nombre, String descripcion)
+        {
+            DataTable tabla = new DataTable();
+            try
+            {
+                conexionstring.Open();
+                OleDbDataAdapter adaptador = new OleDbDataAdapter("Insert into CATEGORIAS (nombre, descripcion) values ('" + nombre + "', '" + descripcion +  "');", conexionstring);
+                adaptador.Fill(tabla);
+                conexionstring.Close();
+                MessageBox.Show("Categoria Creada con exito");
+            }
+            catch (System.Exception error)
+            {
+                MessageBox.Show("Ha habido un error en su conexión, intentelo más tarde");
+            }
+
+        }
+
+
+        public void AgregarEmpleado(String dpi, String nombre, String apellido, String direccion, String fecha_nacimiento, String telefono, String email, String fecha_contratacion, String profesion, String puesto_trabajo, String Salario)
+        {
+            DataTable tabla = new DataTable();
+            try
+            {
+                conexionstring.Open();
+                OleDbDataAdapter adaptador = new OleDbDataAdapter("Insert into EMPLEADOS (dpi,nombre,apellido,direccion, fecha_de_nacimiento, telefono, email, fecha_de_contratacion, profesion, puesto_de_trabajo, salario) ('" + dpi + "','" + nombre + "', '" + apellido + "', '" + direccion + "', '" + fecha_nacimiento + "', '" + telefono + "', '" + email + "','" + fecha_contratacion + "', '" + profesion +"','"+puesto_trabajo +"','"+ Salario +"');", conexionstring);
+                adaptador.Fill(tabla);
+                conexionstring.Close();
+                MessageBox.Show("Empleado Creado con exito");
+            }
+            catch (System.Exception error)
+            {
+                MessageBox.Show("Ha habido un error en su conexión, intentelo más tarde");
+            }
+
+        }
+
+
+
     }
-    
+
 }
